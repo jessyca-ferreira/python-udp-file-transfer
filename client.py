a858dcf92f71c23713bad144a21c3760a9043526
Paulo Rafael Barros de Aguiar <108704252+paulorbaguiar@users.noreply.github.com>
2023-09-20 10:12:19 -0300

    refactor: adciona comentarios

--- client.py
+++ client.py
@@ -4,29 +4,30 @@ import threading
 import sys
 import rdt      # importa o canal rdt criado no aquivo rdt.py
 
-LOCALHOST = socket.gethostbyname(socket.gethostname()) #Endereço de IP local do cliente
-PORT = 5000 #Porta de comunicação
-ORIGIN = (LOCALHOST, random.randint(8000, 9999)) #Tupla com  o LOCALHOST e uma porta de número aleatório entre os valores especificados.
+LOCALHOST = socket.gethostbyname(socket.gethostname()) # endereço de IP local do cliente
+PORT = 5000
+ORIGIN = (LOCALHOST, random.randint(8000, 9999)) # tupla com  o LOCALHOST e uma porta de número aleatório entre os valores especificados.
 
 destination = (LOCALHOST, PORT)
 
-host = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #Criando um socket UDP utilizando o protocolo de rede IPv4
-host.bind(ORIGIN) #Vincula o socket host criado ao endereço e porta na tupla Origin
+host = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # criando um socket UDP utilizando o protocolo de rede IPv4
+host.bind(ORIGIN)
 
-client = rdt.RDTChannel(host) #
-contatos = {}
+client = rdt.RDTChannel(host) # cria um canal de comunicação rdt utilizando o socket
+contatos = {} # dicionário que armazena as informações sobre os contatos
 
+# funçoes receive e send
 def receive():
     while True:
         try:
-            data, address = client.rdt_receive()     # tupla (ack, seq, data)
-            message = data[2]            
+            data, address = client.rdt_receive()     # tupla (ack, seq, data). Recebe dados utilizando o canal rdt3.0
+            message = data[2] #Extrai a mensagem           
             
-            if message == 'bye':
+            if message == 'bye': # usuario desconectado caso a message seja igual a 'bye'
                 print('Você foi desconectado')
                 host.close()
                 break
-            elif message[0] == 'list':       
+            elif message[0] == 'list': # imprime lista de contatos
                 for x in message[1].split('\n'):
                     contatos[x[:14]] = x[17:]
                         
@@ -38,20 +39,21 @@ def receive():
     
 def send():
     while True:
-        message = input()               
-        client.rdt_send(message, destination)
+        message = input() # mensagem a ser enviada               
+        client.rdt_send(message, destination) # envia mensagem pelo canal rdt3.0
         if message == 'bye':
             break
 
-username = ''
+username = '' # recebe nome do usuario
 while not username.startswith('hi, meu nome eh'):
-    username = input('Bem vindo! Digite "hi, meu nome eh <nome_do_usuario>" para se conectar\n')
+    username = input('Bem vindo! Digite "hi, meu nome eh <nome_do_usuario>" para se conectar\n') 
 
-t1 = threading.Thread(target=receive)
+# iniciando threads
+t1 = threading.Thread(target=receive) 
 t2 = threading.Thread(target=send)
 
 t1.start()
 t2.start()
 
-client.rdt_send(username, destination)       
+client.rdt_send(username, destination)  # começa a conexao       
     
